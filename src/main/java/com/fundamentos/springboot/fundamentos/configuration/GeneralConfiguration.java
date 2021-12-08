911b2818850a38b36b1d5fcf02bd752b1ef7cd1e
jamal <carldeltoro@gmail.com>
2021-12-08 11:52:45 -0400

    Configuracion de datasource a traves de properties y clases con h2

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentos.springboot.fundamentos.bean.MyBeanWithPropertiesImplement
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * System:                 CleanBnB
@@ -34,5 +37,16 @@ public class GeneralConfiguration
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name,apellido);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+
+        return dataSourceBuilder.build();
     }
 }
