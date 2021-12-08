99e6f1b40d88b1b81f45efdb1a40fdd4a8e33ef0
jamal <carldeltoro@gmail.com>
2021-12-08 13:41:34 -0400

    uso de anotaciones para obtener los valores del properties

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
@@ -21,6 +22,7 @@ import javax.sql.DataSource;
  * @since 12/8/21
  */
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration
 {
@@ -34,6 +36,15 @@ public class GeneralConfiguration
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${driver}")
+    private String driver;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name,apellido);
@@ -42,10 +53,10 @@ public class GeneralConfiguration
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
 
         return dataSourceBuilder.build();
     }
