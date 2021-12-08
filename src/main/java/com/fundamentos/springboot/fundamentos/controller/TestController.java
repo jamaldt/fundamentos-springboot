2efd109d233a5f7a95a470fa78ebf952413661f4
jamal <carldeltoro@gmail.com>
2021-12-08 04:07:33 -0400

    Cambio del puerto y path

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.fundamentos.springboot.fundamentos.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * System:                 CleanBnB
+ * Name:                   TestController
+ * Description:            Class that represents a TestController's Entity in the application
+ *
+ * @author carlosdeltoro
+ * @version 1.0
+ * @since 12/8/21
+ */
+@Controller
+public class TestController
+{
+    @RequestMapping
+    @ResponseBody
+    public ResponseEntity<String> function(){
+        return  new ResponseEntity<>("hello from controller", HttpStatus.OK);
+    }
+}
