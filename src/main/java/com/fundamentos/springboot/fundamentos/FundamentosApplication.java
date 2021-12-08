c531870abccd5b4c1be13e480818ef7785d6049f
jamal <carldeltoro@gmail.com>
2021-12-08 13:21:25 -0400

    Uso de JPQL para encontrar y ordenar registros

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import java.lang.reflect.Array;
 import java.time.LocalDate;
@@ -56,24 +57,34 @@ public class FundamentosApplication implements CommandLineRunner
 	{
 		//ejemplosAnteriores();
 		saveUsersInDB();
+		getInformartionJpqlFromUser();
 
 	}
 
 	public void saveUsersInDB(){
-		User user1 = new User("maria","miemail", LocalDate.of(2021,03,23));
-		User user2 = new User("maria2","miemail", LocalDate.of(2021,04,23));
-		User user3 = new User("maria4","miemail", LocalDate.of(2021,05,23));
-		User user4 = new User("maria3","miemail", LocalDate.of(2021,07,23));
-		User user5 = new User("maria5","miemail", LocalDate.of(2021,03,23));
-		User user6 = new User("maria6","miemail", LocalDate.of(2021,10,23));
-		User user7 = new User("maria7","miemail", LocalDate.of(2021,11,23));
-		User user8 = new User("maria8","miemail", LocalDate.of(2020,12,23));
-		User user9 = new User("maria9","miemail", LocalDate.of(2021,10,23));
+		User user1 = new User("maria","email1@gmail.com", LocalDate.of(2021,03,23));
+		User user2 = new User("maria2","email2@gmail.com", LocalDate.of(2021,04,23));
+		User user3 = new User("maria4","email3@gmail.com", LocalDate.of(2021,05,23));
+		User user4 = new User("maria3","email4@gmail.com", LocalDate.of(2021,07,23));
+		User user5 = new User("maria5","email5@gmail.com", LocalDate.of(2021,03,23));
+		User user6 = new User("maria6","email6@gmail.com", LocalDate.of(2021,10,23));
+		User user7 = new User("maria7","email7@gmail.com", LocalDate.of(2021,11,23));
+		User user8 = new User("maria8","email8@gmail.com", LocalDate.of(2020,12,23));
+		User user9 = new User("maria9","email9@gmail.com", LocalDate.of(2021,10,23));
 
 		List<User> list = Arrays.asList(user1,user2,user3,user4,user5,user6,user7,user8,user9);
 		list.stream().forEach(_userRepository::save);
 	}
 
+	public void getInformartionJpqlFromUser(){
+		LOGGER.info("user con el metodo encontrado "+
+			_userRepository.findByUserEmail("email8@gmail.com")
+			.orElseThrow(()-> new RuntimeException("no se encontro al user")));
+
+		_userRepository.findAndSort("mari", Sort.by("id").descending())
+			.stream()
+			.forEach(user-> LOGGER.info("User with method sort "+ user));
+	}
 	public void ejemplosAnteriores(){
 		componentDependency.saludar();
 		myBean.print();
--- src/main/java/com/fundamentos/springboot/fundamentos/repository/UserRepository.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.fundamentos.springboot.fundamentos.repository;
 
 import com.fundamentos.springboot.fundamentos.entity.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * System:                 CleanBnB
@@ -16,5 +21,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>
 {
+    @Query("select u from User u Where u.email=?1")
+    Optional<User> findByUserEmail(String email);
 
+    @Query("select u from User u where u.name like ?1%")
+    List<User> findAndSort(String name, Sort sort);
 }
